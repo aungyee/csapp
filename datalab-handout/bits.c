0b300eb049aef7810879e97f69176a1d4ca1b644
aungyee <lyndon.lso@gmail.com>
2025-04-23 01:14:25 +0000

    allOddbits correct. Need to optimise logic to use less operators

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ~(~(~x & y) & ~(x & ~y));
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return 1 << 31;
 }
 //2
 /*
@@ -165,7 +163,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  return !(x ^ ~(1 << 31));
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +174,12 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  int y = !((x & 0xAA) ^ 0xAA);
+  int z = !((x & 0xAA << 8) ^ (0xAA << 8));
+  int xy = !((x & 0xAA << 16) ^ (0xAA << 16));
+  int xz = !((x & 0xAA << 24) ^ (0xAA << 24));
+  int result = y & z & xy & xz;
+  return result;
 }
 /* 
  * negate - return -x 

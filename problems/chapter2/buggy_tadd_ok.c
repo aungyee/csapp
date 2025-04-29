c03d165ae55a65438175868a8fbe9daeba1330df
Aung Yee <lyndon.lso@gmail.com>
2025-04-29 22:21:10 +0100

    Solved problem 2.31

---
+++ problems/chapter2/buggy_tadd_ok.c
@@ -0,0 +1,22 @@
+/* problem 2.31 */
+
+#include <stdio.h>
+#include <assert.h>
+
+/* Determine whether arguments can be added without overflow */
+/* WARNING: This code is buggy. */
+int tadd_ok(int x, int y) {
+  int sum = x + y;
+  return (sum - x == y) && (sum - y == x);
+}
+
+int main() {
+  printf("%d\n", tadd_ok(0x80000000,0x80000000)); // this will return true
+  printf("%x\n", (int)(0x80000000 + 0x80000000)); // because this sum is zero due to overflow
+  printf("%x\n", (int)(0x80000000 + 0x80000000) - (int)(0x80000000)); // the negation of TMin is TMin because of overflow
+
+  /* Therefore, sum - x == y is true in this case which should not be correct */
+
+  return 0;
+}
+

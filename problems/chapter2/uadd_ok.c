16eee03dc07c6cfd2c142983214f18b4dc1d0673
Aung Yee <lyndon.lso@gmail.com>
2025-05-01 00:58:16 +0100

    Integer problems (#6)
    * completed exercise 2.27
    
    * Solved problem 2.30
    
    * clean up
    
    * Solved problem 2.31
    
    * Correction to previous comment
    
    * Fix a bug
    
    * Add more test case

---
+++ problems/chapter2/uadd_ok.c
@@ -0,0 +1,19 @@
+/* Problem 2.27 */
+
+#include <assert.h>
+
+/* Determine whether arguments can be added without overflow */
+int uadd_ok(unsigned x, unsigned y) {
+  unsigned sum = x + y;
+  return !(sum < x || sum < y);
+}
+
+int main() {
+  assert(uadd_ok(0xfffffffa, 0xffffff00) == 0);
+  assert(uadd_ok(0x00000000, 0xffffff00) == 1);
+  assert(uadd_ok(0xffffffff, 0x00000000) == 1);
+  assert(uadd_ok(0xffffffff, 0xffffffff) == 0);
+  assert(uadd_ok(0x00000000, 0x00000000) == 1);
+  
+  return 0;
+}
